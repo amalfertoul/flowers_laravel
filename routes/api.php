ef9163d93ac4b49d9b0d0986fedac40206cfd73a
jaaysus <issaelkaddaoui@gmail.com>
2025-02-14 10:26:38 +0000

    unfinished sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
@@ -9,9 +11,29 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\EventController;
 
-Route::apiResource('users', UserController::class);
-Route::apiResource('products', ProductController::class);
-Route::apiResource('cart', CartController::class);
-Route::apiResource('orders', OrderController::class);
-Route::apiResource('reviews', ReviewController::class);
-Route::apiResource('events', EventController::class);
+
+Route::post('login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+
+    
+    return response()->json(['token' => $user->createToken('Floral Dreams')->plainTextToken]);
+});
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('products', ProductController::class);
+    Route::apiResource('cart', CartController::class);
+    Route::apiResource('orders', OrderController::class);
+    Route::apiResource('reviews', ReviewController::class);
+    Route::apiResource('events', EventController::class);
+});
